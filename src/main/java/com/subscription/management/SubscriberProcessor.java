96278052ae8dea0afa5c31618e9b08f6440c55f4
Maja <maja.bloom@outlook.com>
2026-04-07 10:39:48 +0200

    Add code to SubscriberProcessor

--- src/main/java/com/subscription/management/SubscriberProcessor.java
+++ src/main/java/com/subscription/management/SubscriberProcessor.java
@@ -1,4 +1,21 @@
 package com.subscription.management;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SubscriberProcessor {
+
+    // Filter list based on SubscriberFilter rule
+    public List<Subscriber> findSubscribers(List<Subscriber> list, SubscriberFilter filter) {
+        return list.stream()
+                .filter(s -> filter.matches(s))
+                .collect(Collectors.toList())
+    }
+
+    // Find matching subscribers and perform an action on them
+public List<Subscriber> applyToMatching(List<Subscriber> list, SubscriberFilter filter, SubscriberAction action) {
+        List<Subscriber> matches = findSubscribers(list, filter);
+        matches.forEach(s -> action.run(s));
+        return matches;
+}
 }
